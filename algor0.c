4b173c37060bce6e7b96fd44ed6986c360a8caa3
Mhd Zakarea Alshareef <malshare@1-B-1.42heilbronn.de>
2026-05-14 18:12:40 +0200

    algo1 v1

--- algor0.c
+++ algor0.c
@@ -6,13 +6,56 @@
 /*   By: malshare <malshare@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 17:58:01 by malshare          #+#    #+#             */
-/*   Updated: 2026/05/13 18:49:27 by malshare         ###   ########.fr       */
+/*   Updated: 2026/05/13 22:35:20 by malshare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
 //  first algorithem : check for the minimum element in the stak 
 // chek if it is closer to top or buttom
 // push it to the nearest eadge
 //  push it to stake b
 
+static void	return_to_a(t_stack *a, t_stack *b)
+{
+	int	max_rank;
+	int	pos;
+
+	while (b->size > 0)
+	{
+		max_rank = b->size - 1;
+		pos = find_rank_position(b, max_rank);
+
+		if (pos <= b->size / 2)
+			while (pos-- > 0)
+				rb(b);
+		else
+			while (pos++ < b->size)
+				rrb(b);
+
+		pa(a, b);
+	}
+}
+
+void	simple_algo(t_stack *stack_a, t_stack *stack_b)
+{
+	int	max_rank;
+	int	pos;
+
+	while (stack_a->size > 0)
+	{
+		max_rank = stack_a->size - 1;
+		pos = find_rank_position(stack_a, max_rank);
+
+		if (pos <= stack_a->size / 2)
+			while (pos-- > 0)
+				ra(stack_a);
+		else
+			while (pos++ < stack_a->size)
+				rra(stack_a);
+
+		pb(stack_a, stack_b);
+	}
+	return_to_a(stack_a, stack_b);
+}
